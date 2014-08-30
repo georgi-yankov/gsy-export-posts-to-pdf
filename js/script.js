c209d11413c3e002a6079423d0767c353163c2a0
Georgi Yankov <georgi.st.yankov@gmail.com>
2014-08-30 22:53:43 +0300

    removed attached event

--- js/script.js
+++ js/script.js
@@ -8,28 +8,6 @@
         gsyExportPostsToPdf = $('#gsy-export-posts-to-pdf');
         exportToPdfBtn = $('#submit', gsyExportPostsToPdf);
 
-        // Attach events
-        $('input[type=checkbox]', gsyExportPostsToPdf).click(existCheckedCheckbox);
-
-        function existCheckedCheckbox() {
-            var existChecked;
-
-            existChecked = false;
-
-            $('input[type=checkbox]', gsyExportPostsToPdf).each(function() {
-                if (this.checked === true) {
-                    existChecked = true;
-                    return false;
-                }
-            });
-
-            if (existChecked) {
-                exportToPdfBtn.removeAttr('disabled');
-            } else {
-                exportToPdfBtn.attr('disabled', 'disabled');
-            }
-        }
-
     });
 
 })(jQuery);
