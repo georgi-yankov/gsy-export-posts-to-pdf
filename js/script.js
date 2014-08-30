ccce71ddf938373654840aef05396468c4cd3da4
Georgi Yankov <georgi.st.yankov@gmail.com>
2014-08-30 11:44:27 +0300

    initialized vars and attached event

--- js/script.js
+++ js/script.js
@@ -2,6 +2,33 @@
     'use strict';
 
     $(document).ready(function() {
+        var gsyExportPostsToPdf,
+                exportToPdfBtn;
+
+        gsyExportPostsToPdf = $('#gsy-export-posts-to-pdf');
+        exportToPdfBtn = $('#submit', gsyExportPostsToPdf);
+
+        // Attach events
+        $('input[type=checkbox]', gsyExportPostsToPdf).click(existCheckedCheckbox);
+
+        function existCheckedCheckbox() {
+            var existChecked;
+
+            existChecked = false;
+
+            $('input[type=checkbox]', gsyExportPostsToPdf).each(function() {
+                if (this.checked === true) {
+                    existChecked = true;
+                    return false;
+                }
+            });
+
+            if (existChecked) {
+                exportToPdfBtn.removeAttr('disabled');
+            } else {
+                exportToPdfBtn.attr('disabled', 'disabled');
+            }
+        }
 
     });
 
